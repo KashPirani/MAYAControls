16992d1008d8ae6dc01ebd39f69dbc2b64ba55a9
KashPirani <kash@pirani.net>
2019-07-21 14:51:11 -0400

    fixed serial connection issues

--- ZaberConsole1.py
+++ ZaberConsole1.py
@@ -1,36 +1,30 @@
-import zaber.serial
+from zaber.serial import BinarySerial, BinaryDevice, BinaryCommand, BinaryReply
 import serial
 import serial.tools.list_ports
 
-XDEVPORT = "DEFAULT"
-YDEVPORT = "DEFAULT"
+XYDEVPORT = "COM10"
 ZDEVPORT = "Arduino"
 
 class Maya:
 
     def __init__(self):
         com_ports = list(serial.tools.list_ports.comports())
-        x_com_port = "NULL"
-        y_com_port = "NULL"
+        xy_com_port = "NULL"
         z_com_port = "NULL"
         for port in com_ports:
             print(port)
-            if XDEVPORT in port[1]:
-                x_com_port = port[0]
-            if YDEVPORT in port[1]:
-                y_com_port = port[0]
+            if XYDEVPORT in port[1]:
+                xy_com_port = port[0]
             if ZDEVPORT in port[1]:
                 z_com_port = port[0]
-        if "NULL" == x_com_port:
+        if "NULL" == xy_com_port:
             self.x_axis_device = "NULL"
-            print("Could not find x-axis device")
-        else:
-            self.x_axis_device = BinarySerial(x_com_port, 1)
-        if "NULL" == y_com_port:
             self.y_axis_device = "NULL"
-            print("Could not find y-axis device")
+            print("Could not find xy-axis device")
         else:
-            self.y_axis_device = BinarySerial(y_com_port, 2)
+            port = BinarySerial(xy_com_port)
+            self.x_axis_device = BinaryDevice(port, 1)
+            self.y_axis_device = BinaryDevice(port, 2)
         if "NULL" == z_com_port:
             self.z_axis_device = "NULL"
             print("Could not find z-axis device")
@@ -56,21 +50,21 @@ class Maya:
         elif "y" == axis.lower():
             if "NULL" != self.y_axis_device:
                 if "move_abs" == move_type:
-                    self.y_axis_device.move_abs(position)
+                    self.y_axis_device.move_abs(int(position))
                 elif "move_rel" == move_type:
-                    self.y_axis_device.move_rel(position)
+                    self.y_axis_device.move_rel(int(position))
                 elif "home" == move_type:
-                    self.y_axis_device.home(position)
+                    self.y_axis_device.home()
             else:
                 print("ERROR: y-axis device wasn't found during init")
         elif "x" == axis.lower():
             if "NULL" != self.x_axis_device:
                 if "move_abs" == move_type:
-                    self.x_axis_device.move_abs(position)
+                    self.x_axis_device.move_abs(int(position))
                 elif "move_rel" == move_type:
-                    self.x_axis_device.move_rel(position)
+                    self.x_axis_device.move_rel(int(position))
                 elif "home" == move_type:
-                    self.x_axis_device.home(position)
+                    self.x_axis_device.home()
             else:
                 print("ERROR: x-axis device wasn't found during init")
         else:
@@ -81,6 +75,8 @@ while True:
     user_input = input("command: ").split()
     command = user_input[0]
     axis = user_input[1]
-    position = user_input[2]
+    position = 0
+    if not command == "home":
+        position = user_input[2]
     maya.move_dev(command, axis, position)
     
